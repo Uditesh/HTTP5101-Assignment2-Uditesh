5e1d5b00c25fe832afb3a9110a9774e5469aebff
Uditesh <uditesh24jha@gmail.com>
2021-02-28 22:49:40 +0530

    add another condition

--- HTTP5101-Assignment2-Uditesh/Controllers/J2ProblemController.cs
+++ HTTP5101-Assignment2-Uditesh/Controllers/J2ProblemController.cs
@@ -15,8 +15,10 @@ namespace HTTP5101_Assignment2_Uditesh.Controllers
         ///  which were empty. Today, you recorded the same information.
         ///  How many of the parking spaces were occupied both yesterday and today?
         ///  !!!!!!!!!!!!! NOTE !!!!!!!!!!!!!!!!!!
-        ///  This program is checking only matches of parking of yesterday and today. Just Parking
-        ///  is matched or not. Input should be string only. No special character (like @, . etc)
+        ///  This program is checking only matches of parking of yesterday and today. 
+        ///  If character 'c'(not case sensitive) is matched it will return number of matches.
+        ///  Input should be string only. To match, Special character (like @, . , $ etc) or 
+        ///  charcters other than 'c' should not be typed.
         /// </summary>
         /// <param name="length">Length of the parking</param>
         /// <param name="day1">First Day</param>
@@ -29,10 +31,16 @@ namespace HTTP5101_Assignment2_Uditesh.Controllers
         [Route("api/J2ProblemController/occupy/{length}/{day1}/{day2}")]
         public int OccupyParking(int length, string day1, string day2)
         {
+            string day1Upper = day1.ToUpper();
+            string day2Upper = day2.ToUpper();
+
+            char[] day1chars = day1Upper.ToCharArray();
+            char[] day2chars = day2Upper.ToCharArray();
             int count = 0;
             for(int i=0; i < length; i++)
             {
-                if ((day1[i].Equals(day2[i]))){
+                if ((day1chars[i] == 'C' &&  day1chars[i] == day2chars[i]))
+                {
                     count++;
                 }
             }
